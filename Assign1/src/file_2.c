cf564ebccd6e266e4b7e063896324dc6efeef588
Nihal Nakade <45220716+nihalnakade7@users.noreply.github.com>
2021-01-20 12:52:26 +0530

    Add files via upload

---
+++ Assign1/src/file_2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include "../inc/file_2.h"
+
+
+void print_file2()
+{
+	printf("\n Inside File_2");
+}
---
+++ Assign1/src/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include "../inc/file_1.h"
+#include "../inc/file_2.h"
+int main()
+{
+	print_file1();
+	print_file2();
+	printf("\nEnd");
+	return 0;
+}
+
