44d3a47211423bd66b841555f51fc46082860fbd
nihalnakade7 <nihalnakade7@gmail.com>
2021-01-21 19:03:29 +0530

    Updated file added

---
+++ Assignment1/src/file_2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<file_2.h>
+
+
+void print_file2()
+{
+	printf("\n Inside File_2");
+}
---
+++ Assignment1/src/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<file_1.h>
+#include<file_2.h>
+int main()
+{
+	print_file1();
+	print_file2();
+	printf("\nEnd\n");
+	return 0;
+}
+
